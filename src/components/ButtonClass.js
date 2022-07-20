d44384332a566b1b89e1ecfea9de89c235386fbe
Jarrod Bogard <jarrodbogard@gmail.com>
2022-07-20 18:04:28 -0500

    changed ButtonClass.js file would'nt compile with class constructor

--- src/components/ButtonClass.js
+++ src/components/ButtonClass.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-// export const ButtonClass = ({
-//   label = "Default",
-//   handleClick = () => console.log("I am default"),
-// }) => <button onClick={handleClick}>{label}</button>;
+export const ButtonClass = ({
+  label = "Default",
+  handleClick = () => console.log("I am default"),
+}) => <button onClick={handleClick}>{label}</button>;
 
-export class ButtonClass extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// export class ButtonClass extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-  render() {
-    const {
-      label = "Default Class",
-      handleClick = () => console.log("I am default"),
-    } = this.props;
-    return <button onClick={handleClick}>{label}</button>;
-  }
-}
+//   render() {
+//     const {
+//       label = "Default Class",
+//       handleClick = () => console.log("I am default"),
+//     } = this.props;
+//     return <button onClick={handleClick}>{label}</button>;
+//   }
+// }
